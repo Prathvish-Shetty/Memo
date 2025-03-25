19c9676d13cc0dea47aafa498fb9bcc23d6ca20a
Prathvish-Shetty <142135048+Prathvish-Shetty@users.noreply.github.com>
2025-03-25 17:47:32 +0530

    fixed get started in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import useAuthStore from '../store/useAuthStore';
 
 function Home() {
+  const isAuthenticated = useAuthStore((state) => state.accessToken) != null
+
   return (
     <div
       className="hero min-h-[90vh]"
@@ -15,7 +18,7 @@ function Home() {
           <p className="mb-5 md:text-xl">
           Easily create, manage, and share your memories with Memo. Stay organized and access your thoughts anytime, anywhere!
           </p>
-          <Link to='/signup' className="btn bg-amber-50 text-black border-0 md:text-xl md:btn-xl">Get Started</Link>
+          {!isAuthenticated && <Link to='/signup' className="btn bg-amber-50 text-black border-0 md:text-xl md:btn-xl">Get Started</Link>}
         </div>
       </div>
     </div>
