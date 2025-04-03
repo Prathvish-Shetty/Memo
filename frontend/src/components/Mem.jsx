fdf22d2f0cc0159db629d55791863d6891353c64
Prathvish-Shetty <142135048+Prathvish-Shetty@users.noreply.github.com>
2025-04-03 11:41:18 +0530

    corrected button size in card

--- frontend/src/components/Mem.jsx
+++ frontend/src/components/Mem.jsx
@@ -15,22 +15,25 @@ function Mem({ mem, onEdit, onDelete }) {
   };
 
   return (
-    <div className="card bg-base-200 shadow-md p-4 flex flex-col gap-3 w-70 lg:w-96 sm:w-74 ">
+    <div className="card bg-base-200 shadow-md p-4 flex flex-col gap-3 w-76 lg:w-96 ">
       <h3 className="text-lg font-bold">{mem.title}</h3>
       <p className="h-36 overflow-y-auto">{mem.content}</p>
       <div className="flex justify-center items-center">
-        <div className="flex justify-around gap-4 flex-wrap">
-          <button className="btn btn-md btn-soft w-full sm:w-fit text-blue-400" onClick={onEdit}>
+        <div className="flex justify-around gap-4 flex-wrap w-full">
+          <button className="btn btn-md btn-soft w-full sm:w-auto text-blue-400" onClick={onEdit}>
             Update
           </button>
-          {mem.isPublic && <button className="btn btn-md w-full sm:w-fit btn-soft text-green-400" onClick={handleShare}>
-            Share
-          </button>}
-          <button className="btn btn-md w-full btn-soft sm:w-fit text-red-400" onClick={() => onDelete(mem._id)}>
+          {mem.isPublic && (
+            <button className="btn btn-md btn-soft w-full sm:w-auto text-green-400" onClick={handleShare}>
+              Share
+            </button>
+          )}
+          <button className="btn btn-md btn-soft w-full sm:w-auto text-red-400" onClick={() => onDelete(mem._id)}>
             Delete
           </button>
         </div>
       </div>
+
       {showAlert && <Alert text="Copied to clipboard" />}
     </div>
   );
